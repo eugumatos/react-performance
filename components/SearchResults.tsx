46b6e5a5d217fe053d5f605465c4df236cdb4831
Gustavo <eugumatos@Gustavos-MacBook-Air.local>
2021-08-02 21:17:36 -0300

    Add react virtualized

--- components/SearchResults.tsx
+++ components/SearchResults.tsx
@@ -1,4 +1,5 @@
 import { useMemo } from 'react'
+import { List, ListRowRenderer } from 'react-virtualized';
 import { ProductItem } from './ProductItem';
 
 interface SearchResultsProps {
@@ -13,17 +14,37 @@ interface SearchResultsProps {
 }
 
 export function SearchResults({ results, totalPrice, onAddToWishList }: SearchResultsProps) {
+  const rowRender: ListRowRenderer = ({ index, key, style }) => {
+    return (
+      <div key={key} style={style}>
+        <ProductItem 
+          product={results[index]} 
+          onAddToWishList={onAddToWishList} 
+        />
+      </div>
+    );
+  }
+  
   return (
     <div>
       <h2>{totalPrice}</h2>
 
-      {results.map(product => {
+      <List
+        height={300}
+        rowHeight={30}
+        width={900}
+        overscanRowCount={5}
+        rowCount={results.length}
+        rowRenderer={rowRender}
+      />
+
+      {/* results.map(product => {
         return (
           <>
             <ProductItem key={product.id} product={product} onAddToWishList={onAddToWishList} />
           </>
         );
-      })}
+      }) */}
     </div>
   );
 }
